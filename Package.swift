05ac4a21712f6e9aa75f2e4211279235380a78c7
mdazman <muhdazman88@gmail.com>
2024-06-24 15:15:47 +0800

    Convert to SPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.9
+import PackageDescription
+let package = Package(
+    name: "NYT360Video",
+    defaultLocalization: "en",
+        products: [
+        .library(
+            name: "NYT360Video",
+            targets: ["NYT360Video"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "NYT360Video",
+            dependencies: [],
+            path: "Sources/NYT360Video"),
+        .testTarget(
+            name: "NYT360VideoTests",
+            dependencies: ["NYT360Video"],
+            path: "Tests/NYT360VideoTests"),
+    ]
+)
